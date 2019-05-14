4ecf4a137e05eb342ad49f84b1bad30ed1047208
pascal fertelle <p.fertelle@hotmail.fr>
2019-05-14 06:29:59 +0200

    14/05/19

---
+++ mdp.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Mot de passe</title>
+</head>
+<body>
+	<form method = "post">
+		<legend> Votre mot de passe</legend>
+		<p>password : <input type = "password" name = "password"> </p>
+	</form>
+</body>
+</html>
