a17260e2887cb9770883c3d9995e6c3f2f7b4321
pascalfertelle <p.fertelle@hotmail.fr>
2019-06-17 16:58:23 +0200

    17/06/19 17H00

--- pret_immobilierV2.php
+++ pret_immobilierV2.php
@@ -56,7 +56,7 @@
 			<th>Capital remboursé</th>
 			<th>Capital restant dû</th>
 			<th>date de remboursement</th>';
-			if ($pret1->assurance()=0)
+			if ($_POST['assurance']==0)
 			{echo '<th>Assurance du prêt</th>';}
 			echo '<th>Montant total à rembourser</th></tr>';
 
